fdcc8c70df0a8bdfd82a80f996a9e2f967957750
DavidE1025 <DavidEmory1025@gmail.com>
2014-11-12 01:15:53 -0500

    Created Items and Location differences depending on items

--- locations.js
+++ locations.js
@@ -14,14 +14,15 @@ dispMsg("You walk into the forest. It is dark in every direction and you can hea
 function locBase(){           
 dispMsg("You arrive at the base of the mountain as it looms over head you can see a path to the north that leads up the mountain and to the east is a cave. The beach is to the west.");
 }    
-function locSummit(){
+function locSummit(){ 
 if (sword === false){   
-  dispMsg("You walk up the mountain and arrive at the summit there is a sword on an alter here. You can see the entire island. Past the bridge to the south of the beach you can see a mansion, on the east side of the mountain there is what looks to be a temple surrounded by a forest, and back the west you can see your ship.") 
+  dispMsg("You walk up the mountain and arrive at the summit there is an ancient sword on an alter here. You can see the entire island. Past the bridge to the south of the beach you can see a mansion, on the east side of the mountain there is what looks to be a temple surrounded by a forest, and back the west you can see your ship.") 
 } else { 
-    if (sword === true) 
-dispMsg("You walk up the mountain and arrive at the summit. You can see the entire island. Past the bridge to the south of the beach you can see a mansion, on the other side of the mountain there is what looks to be a temple surrounded by a forest, and back the west you can see your ship."); 
+    if (sword === true){ 
+dispMsg("You walk up the mountain and arrive at the summit this is where you picked up the sword from. You can see the entire island. Past the bridge to the south of the beach you can see a mansion, on the other side of the mountain there is what looks to be a temple surrounded by a forest, and back the west you can see your ship."); 
+    }
   }
-}    
+}      
 function locCave(){           
 dispMsg("You head into the cave but it is to dark to continue, you need some source of light.");					  
 }    
@@ -54,5 +55,5 @@ function locError(){
 dispMsg("Stop, you cannot go that way.");
 }
 function locHelp(){ 
-dispMsg("To move use the directional buttons below or type N, S, E, W, or even n, s, e, w. To take items use type T, and to view your inventory type I or use th einventory button.")
+dispMsg("To move use the directional buttons below or type N, S, E, W, or even n, s, e, w. To take items use type T, and to view your inventory type I or use the inventory button.")
 } 
